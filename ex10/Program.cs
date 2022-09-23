ea154cdb8809215c304b54a984de25c6d2a53c82
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-23 22:04:42 +0300

    ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,15 @@
+﻿int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void PrintData(string prefix, string value)
+ {
+    Console.WriteLine(prefix + value);
+ }  
+int number=ReadData("Введите трехзначное число: ");
+int num2=(number/10)%10;
+PrintData("Вторая цифра в этом числе: ", num2.ToString());
+ 
