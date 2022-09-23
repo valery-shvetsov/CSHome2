c9aeb0153175501cc0da01175738974660075ea8
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-23 23:28:14 +0300

    ex12

---
+++ ex12/Program.cs
@@ -0,0 +1,29 @@
+﻿int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void PrintData(string prefix, string value)
+ {
+    Console.WriteLine(prefix + value);
+ }
+
+ int number = ReadData("Введите номер дня недели (от 1 до 7): "); 
+ 
+ if (number>=1 && number<=7)
+{
+    if (number>=6 && number<=7)
+    {
+       PrintData("выходной", "");  
+    }
+    else
+    {
+        PrintData("рабочий", "");
+    }
+}
+else
+{    
+    PrintData("Номер дня недели должен быть от 1 до 7", ""); 
+}
