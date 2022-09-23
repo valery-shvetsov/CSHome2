4afb73066a8eee5f71adf44376ba0fd86299d1df
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-23 23:00:26 +0300

    ex11

---
+++ ex11/Program.cs
@@ -0,0 +1,28 @@
+﻿int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void PrintData(string prefix, string value)
+ {
+    Console.WriteLine(prefix + value);
+ }
+
+ int number = ReadData("Введите число более 99: ");
+  if (number < 100)
+   {     
+    PrintData("Вы ввели число менее 100 и третьей цифры в числе нет", ""); 
+    } 
+else
+{
+int num3=0;
+while (number>1000)
+{
+    number=number/10;
+}
+    num3=number%10;
+    PrintData("Третья цифра в числе: ", num3.ToString());
+}
+
